3716bf528280907b082a27acd845de76bf6c6bf8
arnolf <afayolle@****.***>
2014-05-29 02:03:28 +0200

    ajout demo angular

--- angular-demo/js/Controllers/controllers.js
+++ angular-demo/js/Controllers/controllers.js
@@ -3,15 +3,14 @@ define([], function () {
     var controllers = {};
 
     controllers.index = function ($scope, $compile, $interval) {
-        var img1 = "<img src='img1.jpg'/>";
-        var img2 = "<img src='img2.jpg'/>";
-        $scope.content = img1;
+        var index = 0;
+        var contents = ["<img src='img1.jpg'/>", "<img src='img2.jpg'/>", "<div style='height:100%;background-color:blue;'>Hello {{name}} !</div>"];
+        $scope.name = 'World !';
+        $scope.content = contents[index];
         $interval(function() {
-            if ($scope.content == img1) {
-                $scope.content = img2;
-            } else {
-                $scope.content = img1;
-            }
+           index++
+           if (index >= contents.length) index = 0;
+            $scope.content = contents[index];
         }, 3000);
         
     }
