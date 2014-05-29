ccc125dbf19ea3a617c5e341392d60ea8ed2b342
arnolf <afayolle@****.***>
2014-05-29 02:04:48 +0200

    indentation js

--- angular-demo/js/Controllers/controllers.js
+++ angular-demo/js/Controllers/controllers.js
@@ -7,14 +7,14 @@ define([], function () {
         var contents = ["<img src='img1.jpg'/>", "<img src='img2.jpg'/>", "<div style='height:100%;background-color:blue;'>Hello {{name}} !</div>"];
         $scope.name = 'World !';
         $scope.content = contents[index];
-        $interval(function() {
-           index++
-           if (index >= contents.length) index = 0;
+        $interval(function () {
+            index++
+            if (index >= contents.length) index = 0;
             $scope.content = contents[index];
         }, 3000);
-        
+
     }
-    
+
     return controllers;
 
 });
