dcacc3a7c48f4303d9945f565dbf332bc359c67c
arnolf <afayolle@****.***>
2014-05-29 12:07:00 +0200

    changement pic

--- angular-demo/js/Controllers/controllers.js
+++ angular-demo/js/Controllers/controllers.js
@@ -4,7 +4,7 @@ define([], function () {
 
     controllers.index = function ($scope, $compile, $interval) {
         var index = 0;
-        var contents = ["<img src='img1.jpg'/>", "<img src='img2.jpg'/>", "<div style='height:100%;background-color:blue;'>Hello {{name}} !</div>"];
+        var contents = ["<img src='img1.jpg' height='100%' width='100%'/>", "<img src='img2.jpg' height='100%' width='100%'/>", "<div style='height:100%;background-color:blue;'>Hello {{name}} !</div>"];
         $scope.options = { 'vertical-overflow-hidden' : true };
         $scope.name = 'World !';
         $scope.content = contents[index];
