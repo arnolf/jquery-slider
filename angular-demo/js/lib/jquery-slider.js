2f5ea0e143831d4ed5f76c47ee19de967d38d364
arnolf <afayolle@****.***>
2014-06-07 12:55:37 +0200

    queuing sliding animation

--- angular-demo/js/lib/jquery-slider.js
+++ angular-demo/js/lib/jquery-slider.js
@@ -28,72 +28,76 @@
     };
 
     $.fn.slide = function (content, direction, duration, easing, callback) {
-        var $wrapper = $(this).find('.wrapper:first');
-        var $page = $wrapper.find('div:first');
-        if ($page.children().length === 0) {
-        	$page.hide().append(content).fadeIn(duration);
-            if (callback != undefined) {
-                callback();
-            }
-        } else {
-            var width = parseInt(this.css('width'));
-            $(this).on({
-                'mousewheel': function (e) {
-                    e.preventDefault();
-                    e.stopPropagation();
-                },
-                'keydown': function (e) {
-                    e.preventDefault();
-                    e.stopPropagation();
-                }
-            });
-            var $this = $(this);
-            var $oldPage = $wrapper.find('div:first');
-            var newPage = document.createElement('div');
-            var $newPage = $(newPage);
-            var scrollLeft;
-            
-            if (direction == 'left') {
-                $wrapper.append(newPage);
-                $newPage.html(content);
-                $newPage.css('width', width);
-                $newPage.css('height', '100%');
-                $newPage.css('left', width);
-                $newPage.css('top', $wrapper.scrollTop());
-                $newPage.css('position', 'absolute');
-                scrollLeft = width;
-            }
-            
-            if (direction == 'right') {
-            	$oldPage.css('left', width);
-                $wrapper.scrollLeft(width);
-                $wrapper.prepend(newPage);
-                $newPage.html(content);
-                $newPage.css('width', width);
-                $newPage.css('height', '100%');
-                $newPage.css('left', 0);
-                $newPage.css('top', $wrapper.scrollTop());
-                $newPage.css('position', 'absolute');
-                scrollLeft = 0;
-            }
-            
-            $wrapper.animate({
-                scrollLeft: scrollLeft
-            }, duration, easing, function () {
-                $oldPage.remove();
-                $newPage.css('left', 0);
-                $newPage.css('top', 0);
-                $(this).scrollTop(0);
-                $(this).scrollLeft(0);
-                $this.off({
-                    'mousewheel': undefined,
-                    'keydown': undefined
-                });
-                if (callback != undefined) {
-                    callback();
-                }
-            });
-        }
+        var $this = $(this);
+    	$(this).queue(function () {
+	        var $wrapper = $this.find('.wrapper:first');
+	        var $page = $wrapper.find('div:first');
+	        if ($page.children().length === 0) {
+	        	$page.hide().append(content).fadeIn(duration);
+	            if (callback != undefined) {
+	                callback();
+	            }
+                $this.dequeue();
+	        } else {
+	            var width = parseInt($this.css('width'));
+	            $this.on({
+	                'mousewheel': function (e) {
+	                    e.preventDefault();
+	                    e.stopPropagation();
+	                },
+	                'keydown': function (e) {
+	                    e.preventDefault();
+	                    e.stopPropagation();
+	                }
+	            });
+	            var $oldPage = $wrapper.find('div:first');
+	            var newPage = document.createElement('div');
+	            var $newPage = $(newPage);
+	            var scrollLeft;
+	            
+	            if (direction == 'left') {
+	                $wrapper.append(newPage);
+	                $newPage.html(content);
+	                $newPage.css('width', width);
+	                $newPage.css('height', '100%');
+	                $newPage.css('left', width);
+	                $newPage.css('top', $wrapper.scrollTop());
+	                $newPage.css('position', 'absolute');
+	                scrollLeft = width;
+	            }
+	            
+	            if (direction == 'right') {
+	            	$oldPage.css('left', width);
+	                $wrapper.scrollLeft(width);
+	                $wrapper.prepend(newPage);
+	                $newPage.html(content);
+	                $newPage.css('width', width);
+	                $newPage.css('height', '100%');
+	                $newPage.css('left', 0);
+	                $newPage.css('top', $wrapper.scrollTop());
+	                $newPage.css('position', 'absolute');
+	                scrollLeft = 0;
+	            }
+	            
+	            $wrapper.animate({
+	                scrollLeft: scrollLeft
+	            }, duration, easing, function () {
+	                $oldPage.remove();
+	                $newPage.css('left', 0);
+	                $newPage.css('top', 0);
+	                $(this).scrollTop(0);
+	                $(this).scrollLeft(0);
+	                $this.off({
+	                    'mousewheel': undefined,
+	                    'keydown': undefined
+	                });
+	                if (callback != undefined) {
+	                    callback();
+	                }
+	                $this.dequeue();
+	            });
+	        }
+    	});
     };
 
 }(jQuery));
